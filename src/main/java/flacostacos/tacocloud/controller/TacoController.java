d32b96d0ce5424ef94d41f09fbdbaf152f4e7428
Andrew Scalise <paradigmcomplex@gmail.com>
2019-06-10 09:54:49 -0500

    Taco controller and thymeleaf template filled out

--- src/main/java/flacostacos/tacocloud/controller/TacoController.java
+++ src/main/java/flacostacos/tacocloud/controller/TacoController.java
@@ -1,4 +1,4 @@
-package flacostacos.tacocloud;
+package flacostacos.tacocloud.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/test/java/flacostacos/tacocloud/TacoControllerTest.java
+++ src/test/java/flacostacos/tacocloud/TacoControllerTest.java
@@ -1,5 +1,6 @@
 package flacostacos.tacocloud;
 
+import flacostacos.tacocloud.controller.TacoController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
